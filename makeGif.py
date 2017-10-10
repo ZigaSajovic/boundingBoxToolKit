da44bbb3d826c665cf59df120a2e1fc2e54f745c
ZigaSajovic <ziga.sajovic@gmail.com>
2017-10-10 05:38:06 +0200

    makeGif.py

--- makeGif.py
+++ makeGif.py
@@ -2,34 +2,47 @@ import maps
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 from matplotlib.animation import FuncAnimation
 import matplotlib.image as mpimg
 
 A=np.loadtxt("/bag/groundtruthAffine.txt",delimiter=',')
-B=np.loadtxt("/groundtruthReoriented.txt",delimiter=',')
+B=np.loadtxt("/bag/groundtruthReorientedWithContext.txt",delimiter=',')
+A2=np.loadtxt("/bag/groundtruthAffineWithContext.txt",delimiter=',')
+C=np.loadtxt("/bag/groundtruthReoriented.txt",delimiter=',')
 
 fig = plt.figure()
-
-gs=plt.GridSpec(3,3)
+gs=plt.GridSpec(2,3)
 ax1=plt.subplot(gs[:,0:2])
-ax2=plt.subplot(gs[1,2])
-line=plt.Polygon(B[0].reshape((4,2)), fill=None, closed=True)
+ax2=plt.subplot(gs[0,2])
+ax3=plt.subplot(gs[1,2])
+line=plt.Polygon(B[0].reshape((4,2)), color="blue", fill=None, closed=True)
+line2=plt.Polygon(C[0].reshape((4,2)),color="red", fill=None, closed=True)
+
 ax1.set_title("boundingBox",fontsize=16)
 ax2.set_title("affine sample",fontsize=16)
+ax3.set_title("affine sample\n with context",fontsize=16)
+
 def init():
- a=mpimg.imread("../vot2016/bag/%08d.jpg"%1)
+ a=mpimg.imread("/vot2016/bag/%08d.jpg"%1)
  ax1.imshow(a)
  ax1.add_line(line)
+ ax1.add_line(line2)
 
 def update(i):
-	a=mpimg.imread("/home/ziga/Downloads/vot2016/bag/%08d.jpg"%i)
+	a=mpimg.imread("/vot2016/bag/%08d.jpg"%i)
 	ax1.imshow(a)
 	ax2.imshow(maps.sampleGrid(A[i-1].reshape((3,3)), a, (40,60)))
+	ax3.imshow(maps.sampleGrid(A2[i-1].reshape((3,3)), a, (40,60)))
 	line.set_xy(B[i-1].reshape((4,2)))
+	line2.set_xy(C[i-1].reshape((4,2)))
 anim = FuncAnimation(fig, update, init_func=init, frames=np.arange(1, A.shape[0]), interval=200)
-
 for i, ax in enumerate(plt.gcf().axes):
 	ax.axis('off')
-#anim.save('animation.gif', fps=30, extra_args=['-vcodec', 'h264', '-pix_fmt', 'yuv420p'])
-anim.save('animation3.gif', dpi=80, writer='imagemagick')
+
+#anim.save('animation.mp4', fps=30, extra_args=['-vcodec', 'h264', '-pix_fmt', 'yuv420p'])
+red_patch = mpatches.Patch(color='red', label='The red data')
+blue_patch = mpatches.Patch(color='blue', label='The blue data')
+fig.legend(handles=[blue_patch,red_patch], labels=["bBox with Context 1.5","boundingBox"], loc="lower center")
+#anim.save('animation4.gif', dpi=80, writer='imagemagick')
 plt.show()
